a4070dfccc86d9e24056c3438abcd50180366186
Pier-Yves Lessard <py.lessard@gmail.com>
2023-06-11 21:32:48 -0400

    Squash of work

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 "use strict"
 
-var gulp = require("gulp")
-var sass = require("gulp-sass")(require("sass"))
-var sourcemaps = require("gulp-sourcemaps")
-var fileinclude = require("gulp-file-include")
-var autoprefixer = require("gulp-autoprefixer")
-var rimraf = require("rimraf")
+const gulp = require("gulp")
+const replace = require("gulp-replace")
+const sass = require("gulp-sass")(require("sass"))
+const sourcemaps = require("gulp-sourcemaps")
+const fileinclude = require("gulp-file-include")
+const autoprefixer = require("gulp-autoprefixer")
+const rimraf = require("rimraf")
 
-var path = {
+const path = {
     src: {
         html: "source/pages/**/*.html",
         others: "source/*.+(php|ico|png)",
@@ -17,6 +18,7 @@ var path = {
         js: "source/js/*.js",
         scss: "source/scss/**/*.scss",
         images: "source/images/**/*.+(png|jpg|gif|svg)",
+        videos: "source/videos/**/*.+(mp4|ogg|avi|mkv)",
     },
     build: {
         dirDev: "dist/",
@@ -27,6 +29,7 @@ var path = {
 gulp.task("html:build", function () {
     return gulp
         .src(path.src.html)
+        .pipe(replace(/<!--\s*prettier-ignore\s*-->\r?\n?/g, ""))
         .pipe(
             fileinclude({
                 basepath: path.src.incdir,
@@ -56,6 +59,11 @@ gulp.task("images:build", function () {
     return gulp.src(path.src.images).pipe(gulp.dest(path.build.dirDev + "images/"))
 })
 
+// Videos
+gulp.task("videos:build", function () {
+    return gulp.src(path.src.videos).pipe(gulp.dest(path.build.dirDev + "videos/"))
+})
+
 // Plugins
 gulp.task("plugins:build", function () {
     return gulp.src(path.src.plugins).pipe(gulp.dest(path.build.dirDev + "plugins/"))
@@ -71,16 +79,9 @@ gulp.task("clean", function (cb) {
     rimraf(path.build.dirDev, cb)
 })
 
-// Watch Task
-gulp.task("watch", function () {
-    gulp.watch(path.src.html, gulp.series("html:build"))
-    gulp.watch(path.src.htminc, gulp.series("html:build"))
-    gulp.watch(path.src.scss, gulp.series("scss:build"))
-    gulp.watch(path.src.js, gulp.series("js:build"))
-    gulp.watch(path.src.images, gulp.series("images:build"))
-    gulp.watch(path.src.plugins, gulp.series("plugins:build"))
-})
-
 // Build Task
-gulp.task("build", gulp.series("clean", "html:build", "js:build", "scss:build", "images:build", "plugins:build", "others:build"))
+gulp.task(
+    "build",
+    gulp.series("clean", "html:build", "js:build", "scss:build", "images:build", "videos:build", "plugins:build", "others:build")
+)
 gulp.task("default", gulp.series("build"))
